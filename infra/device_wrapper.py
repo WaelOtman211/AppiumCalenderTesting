8eb4d1cdbf88ef2e8900094363b08e15349192b9
WaelOtman211 <105595561+WaelOtman211@users.noreply.github.com>
2024-03-11 15:31:47 +0200

    updates

--- infra/device_wrapper.py
+++ infra/device_wrapper.py
@@ -1,21 +1,14 @@
 import selenium
 from selenium import webdriver
 from appium import webdriver
-
 from infra.base_page import BasePage
+from infra.config_reader import get_config_data
 
 
 class BrowserWrapper(BasePage):
     def __init__(self):
         super().__init__(self.get_driver())
-        desired_caps = {
-            'platformName': 'Android',
-            'platformVersion': '11',
-            'deviceName': 'emulator-5554',
-            'appPackage': 'com.claudivan.taskagenda',
-            'appActivity': '.Activities.MainActivity',
-            'automationName': 'UiAutomator2'
-        }
+        desired_caps = get_config_data()
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
         self.driver.implicitly_wait(60)
 
